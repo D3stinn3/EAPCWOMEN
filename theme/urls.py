4243c3db9a9b2dea7590d8efddc3c0e539e2f07f
D3stinn3 <paynedestinne@gmail.com>
2024-01-04 22:10:08 +0300

    New App

--- theme/urls.py
+++ theme/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from theme.views import landingPage, eventsPage, membershipPage, helpPage, homePage
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', homePage, name="homepage"),
@@ -9,4 +7,4 @@ urlpatterns = [
     path('eventspage/', eventsPage, name="eventspage"),
     path('membershippage/', membershipPage, name ="membershippage"),
     path('helppage/', helpPage, name="helppage"),
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+]
